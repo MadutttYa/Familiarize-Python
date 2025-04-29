d77134e559f58a3af3d1a0a41d3a6c892663af65
michael-aditya <michaeladitya@student.ub.ac.id>
2025-04-29 15:29:45 +0700

    Adding generate random jokes from icanhazdadjoke.com API

--- bot.py
+++ bot.py
@@ -21,15 +21,13 @@ async def load_commands():
     for filename in os.listdir("./prefix_command"):
         if (filename.endswith(".py")) and (filename != "__init__.py"):
             await bot.load_extension(f"prefix_command.{filename[:-3]}")
-            print(f"Loaded prefix {filename[:-3]}")
 
 @bot.event
 async def on_ready():
     await load_commands()
     try:
         synced = await bot.tree.sync()
-        for i in range(len(synced)):
-            print(f"Synced {synced[i]} command")
+        print(f"Synced {len(synced)} command(s)")
     except Exception as e:
         print(f"Failed to sync command(s): {e}")
 
