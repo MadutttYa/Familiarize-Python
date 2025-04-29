486440dcc4b25069fba3c9008e7540fa423bd26c
michael-aditya <michaeladitya@student.ub.ac.id>
2025-04-29 15:20:50 +0700

    maybe_last?

--- prefix_command/__init__.py
+++
@@ -1,9 +0,0 @@
-import os
-
-count = 0
-
-for filename in os.listdir("C:/Users/Adit/code/PERSONAL/piton/bot-discord/prefix_command"):
-    if (filename.endswith(".py")) and (filename != "__init__.py"):
-        count += 1
-
-print(f"Loaded {count} prefix commands")
